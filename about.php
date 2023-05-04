6e96ad290c05a5d6438110f54e7129aa4d20a4a7
Ibrahim Bakari <ibrahimwali4u@gmail.com>
2023-05-05 00:21:45 +0100

    base

--- about.php
+++ about.php
@@ -31,13 +31,10 @@
 					EncryptChat is a PHP and Javascript based chat with end-to-end encryption.
 					The database will only contain your encrypted messages, and will have no knowledge of the decryption key.
 					Usernames are also encrypted in the database.
-					Encryption is provided by the <a href="https://github.com/bitwiseshiftleft/sjcl" target="_blank">Stanford Javascript Crypto Library</a>, using 256-bit AES-GCM.
+					Encryption is provided using 256-bit AES-GCM.
 					<br/>
 					<br/>
-					The source-code for the website is available on <a href="https://github.com/Undone/mycryptochat" target="_blank">GitHub</a>
-					<br/>
-					<br/>
-					This is a rework of <a href="https://github.com/HowTommy/mycryptochat" target="_blank">EncryptChat v1.0.4 by HowTommy</a>
+					
 				</p>
 				<h3>Usage</h3>
 				<p>
@@ -50,7 +47,7 @@
 					When the chatroom is created with a random <span class="key">key</span>, the <span class="key">key</span> will be stored in the URL itself.
 					It will look something like this;
 					<br/><br/>
-					mycryptochat.org/chatroom.php?id=<span class="roomid">27SJrBVkQCsQFaCnjU94</span>#<span class="key">1BZX3QOXF78qq0r9HgZk1AeZK-sKkX3VZVKf40VdE6A</span>
+					localhost/encryptchat/chatroom.php?id=<span class="roomid">27SJrBVkQCsQFaCnjU94</span>#<span class="key">1BZX3QOXF78qq0r9HgZk1AeZK-sKkX3VZVKf40VdE6A</span>
 					<br/><br/>
 					The <span class="key">purple</span> part is the <span class="key">key</span> itself encoded with base64.
 					<span class="roomid">Green</span> part is the <span class="roomid">room ID</span>.
@@ -59,14 +56,12 @@
 					The URL has a hashtag before the <span class="key">key</span> so it won't be sent to the server. Even if you have access log enabled on the web server,
 					only the <span class="roomid">room ID</span> will be seen in the logs.
 					<br/><br/>
-					If you want to host EncryptChat yourself, and maintain user privacy. I recommend you disable
-					access logs on your web server. You can connect IP addresses to the right rooms if you don't the disable access logs. EncryptChat itself does not log or use IP addresses
-					in any way.
+					
 				</p>
 			</section>
 		</div>
 		<footer>
-			<p>&copy; 2023 EncryptChat <?php echo EncryptCHAT_VERSION; ?> by <a href="https://github.com/Undone/mycryptochat">Undone</a></p>
+			<p>&copy; 2022 EncryptChat </p>
 		</footer>
 	</body>
 </html>
--- stats.php
+++ stats.php
@@ -11,7 +11,7 @@ $dbManager = new DbManager();
 <html lang="en">
 <head>
 	<meta charset="utf-8" />
-	<title>Stats - MyCryptoChat</title>
+	<title>Stats - EncryptChat</title>
 	<link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
 	<meta name="viewport" content="width=device-width" />
 	<link href="style.css" rel="stylesheet" />
@@ -21,7 +21,7 @@ $dbManager = new DbManager();
 		<div class="content-wrapper">
 			<nav>
 				<ul>
-					<li id="site-title"><a href="index.php">MyCryptoChat</a></li>
+					<li id="site-title"><a href="index.php">EncryptChat</a></li>
 					<div id="site-links">
 						<li><a href="stats.php">Stats</a></li>
 						<li><a href="about.php">About</a></li>
@@ -32,7 +32,7 @@ $dbManager = new DbManager();
 	</header>
 	<div id="body">
 		<section class="content-wrapper main-content clear-fix">
-			<h2>Stats about MyCryptoChat</h2>
+			<h2>Stats about EncryptChat</h2>
 			<p>
 				Number of chat rooms: <?php echo $dbManager->countChatrooms(); ?><br />
 				Number of messages: <?php echo $dbManager->countMessages(); ?>
@@ -40,7 +40,7 @@ $dbManager = new DbManager();
 		</section>
 	</div>
 	<footer>
-		<p>&copy; 2018 MyCryptoChat <?php echo MYCRYPTOCHAT_VERSION; ?> by <a href="https://github.com/Undone/mycryptochat">Undone</a></p>
+		<p>&copy; 2022 EncryptChat </p>
 	</footer>
 </body>
 </html>
